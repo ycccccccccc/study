ae5fb374a205e5c4b84a083e196149719d06e727
ycccccccccc <1078988764@qq.com>
2018-12-17 21:44:59 +0800

    12.17

--- tensorflow/mnist.py
+++ tensorflow/mnist.py
@@ -14,71 +14,140 @@ import collections
 
 slim = tf.contrib.slim
 
-class Block(collections.namedtuple('Block',['scope','unit_fc','args'])):
+
+class Block(collections.namedtuple('Block', ['scope', 'unit_fc', 'args'])):
     'A named tuple describing a ResNet block.'
 
-def subsample(inputs,factor,scope = None):
+
+def subsample(inputs, factor, scope=None):
     if factor == 1:
         return inputs
     else:
-        return slim.max_pool2d(inputs,[1,1],stride = factor,scope = scope)
+        return slim.max_pool2d(inputs, [1, 1], stride=factor, scope=scope)
 
-def conv2d_same(inputs,num_outputs,kernel_size,stride,scope = None):
+
+def conv2d_same(inputs, num_outputs, kernel_size, stride, scope=None):
     if stride == 1:
-        return slim.conv2d(inputs,num_outputs,kernel_size,stride = 1,
-                           padding = 'VALID',scope = scope)
+        return slim.conv2d(inputs, num_outputs, kernel_size, stride=1,
+                           padding='VALID', scope=scope)
     else:
         pad_total = kernel_size - 1
         pad_beg = pad_total // 2
         pad_end = pad_total - pad_beg
-        inputs = tf.pad(inputs,[[0,0],[pad_beg,pad_end],[pad_beg,pad_end],[0,0]])
-        return slim.conv2d(inputs,num_outputs,kernel_size,stride = stride,
-                           padding = 'VALID',scope = scope)
+        inputs = tf.pad(inputs, [[0, 0], [pad_beg, pad_end], [pad_beg, pad_end], [0, 0]])
+        return slim.conv2d(inputs, num_outputs, kernel_size, stride=stride,
+                           padding='VALID', scope=scope)
+
 
 @slim.add_arg_scope
-def stack_blocks_dense(net,blocks,outputs_collections=None):
+def stack_blocks_dense(net, blocks, outputs_collections=None):
     for block in blocks:
-        with tf.variable_scope(block.scope,'block',[net]) as sc:
-            for i,unit in enumerate(block.args):
-                with tf.variable_scope('unit_%d' % (i+1),values=[net]):
-                    unit_depth,unit_depth_bottleneck,unit_stride = unit
+        with tf.variable_scope(block.scope, 'block', [net]) as sc:
+            for i, unit in enumerate(block.args):
+                with tf.variable_scope('unit_%d' % (i + 1), values=[net]):
+                    unit_depth, unit_depth_bottleneck, unit_stride = unit
                     net = block.unit_fc(net,
-                                        depth = unit_depth,
-                                        depth_bottleneck = unit_depth_bottleneck,
-                                        stride = unit_stride)
-            net = slim.utils.collect_named_outputs(outputs_collections,sc.name,net)
+                                        depth=unit_depth,
+                                        depth_bottleneck=unit_depth_bottleneck,
+                                        stride=unit_stride)
+            net = slim.utils.collect_named_outputs(outputs_collections, sc.name, net)
     return net
 
-def resnet_arg_scope(is_training = True,
-                     weight_decay = 0.0001,
-                     batch_norm_decay = 0.997,
-                     batch_norm_epsilon = 1e-5,
-                     batch_norm_scale = True):
+
+def resnet_arg_scope(is_training=True,
+                     weight_decay=0.0001,
+                     batch_norm_decay=0.997,
+                     batch_norm_epsilon=1e-5,
+                     batch_norm_scale=True):
     batch_norm_params = {
-        'is_training':is_training,
-        'decay':batch_norm_decay,
-        'epsilon':batch_norm_epsilon,
-        'scale':batch_norm_scale,
-        'update_collections':tf.GraphKeys.UPDATE_OPS,
+        'is_training': is_training,
+        'decay': batch_norm_decay,
+        'epsilon': batch_norm_epsilon,
+        'scale': batch_norm_scale,
+        'update_collections': tf.GraphKeys.UPDATE_OPS,
     }
     with slim.arg_scope(
-        [slim.conv2d],
-        weights_regularizer = slim.l2_regularizer(weight_decay),
-        weights_initializer = slim.variance_scaling_initializer(),
-        activation_fc = tf.nn.relu,
-        normalizer_fn = slim.batch_norm,
-        normalizer_params = batch_norm_params) :
-        with slim.arg_scope([slim.batch_norm],**batch_norm_params):
-            with slim.arg_scope([slim.max_pool2d],padding = 'SAME') as arg_sc:
+            [slim.conv2d],
+            weights_regularizer=slim.l2_regularizer(weight_decay),
+            weights_initializer=slim.variance_scaling_initializer(),
+            activation_fc=tf.nn.relu,
+            normalizer_fn=slim.batch_norm,
+            normalizer_params=batch_norm_params):
+        with slim.arg_scope([slim.batch_norm], **batch_norm_params):
+            with slim.arg_scope([slim.max_pool2d], padding='SAME') as arg_sc:
                 return arg_sc
 
 
+@slim.add_arg_scope
+def bottleneck(inputs, depth, depth_bottleneck, stride,
+               outputs_collectoins=None, scope=None):
+    with tf.variable_scope(scope, 'bottleneck_v2', [inputs]) as sc:
+        depth_in = slim.utils.last_dimension(inputs.get_shape(), min_rank=4)
+        preact = slim.batch_norm(inputs, activation_fn=tf.nn.relu,
+                                 scope='preact')
+        if depth == depth_in:
+            shortcut = subsample(inputs, stride, 'shortcut')
+        else:
+            shortcut = slim.conv2d(preact, depth, [1, 1], stride=stride,
+                                   normalizer_fn=None, activation_fn=None,
+                                   scope='shortcut')
+        residual = slim.conv2d(preact, depth_bottleneck, [1, 1], stride=1,
+                               scope='conv1')
+        residual = conv2d_same(residual, depth_bottleneck, 3, stride,
+                               scope='conv2')
+        residual = slim.conv2d(residual, depth, [1, 1], stride=1,
+                               normalizer_fn=None, activation=None,
+                               scope='conv3')
+        output = shortcut + residual
+
+        return slim.utils.collect_named_outputs(outputs_collectoins,
+                                                sc.name, output)
 
 
+def resnet_v2(inputs,
+              blocks,
+              num_classes=None,
+              global_pool=True,
+              include_root_block=True,
+              reuse=None,
+              scope=None):
+    with tf.variable_scope(scope, 'resnet_v2', [inputs], reuse=reuse) as sc:
+        end_points_collection = sc.original_name_scope + '_end_points'
+        with slim.arg_scope([slim.con2d, bottleneck, stack_blocks_dense],
+                            outputs_collections=end_points_collection):
+            net = inputs
+            if include_root_block:
+                with slim.arg_scope([slim.conv2d], activation_fn=None,
+                                    normalizer_fn=None):
+                    net = conv2d_same(net, 64, 7, 2, 'conv1')
+                net = slim.max_pool2d(net, [3, 3], stride=2, scope='pool1')
+            net = stack_blocks_dense(net, blocks)
+            net = slim.batch_norm(net, activation_fc=tf.nn.relu, scope='postnorm')
+            if global_pool:
+                net = tf.reduce_mean(net, [1, 2], name='pool5', keep_dims=True)
+            if num_classes is not None:
+                net = slim.conv2d(net, num_classes, [1, 1], actvation_fn=None,
+                                  normalizer_fn=None, scope='logits')
+            end_points = slim.utils.convert_collection_to_dict(end_points_collection)
+            if num_classes is not None:
+                end_points['predictions'] = slim.softmax(net, scope='predictions')
+            return net, end_points
 
 
+def resnet_v2_50(inputs,
+                 num_classes=None,
+                 global_pool=True,
+                 reuse=None,
+                 scope='resnet_v2_50'):
+    blocks = [
+        Block('block1', bottleneck, [(256, 64, 1)] * 2 + [(256, 64, 2)]),
+        Block('block1', bottleneck, [(512, 128, 1)] * 2 + [(512, 128, 2)]),
+        Block('block1', bottleneck, [(1024, 256, 1)] * 2 + [(1024, 256, 2)]),
+        Block('block1', bottleneck, [(2048, 512, 1)] * 3)]
+    return resnet_v2(inputs,blocks,num_classes,global_pool,
+                     include_root_block=True,reuse= reuse,scope = scope)
 
-def time_tensorflow_run(session, target,info_string):
+def time_tensorflow_run(session, target, info_string):
     num_steps_burn_in = 10
     total_duration = 0.0
     toatl_duration_squared = 0.0
@@ -99,23 +168,24 @@ def time_tensorflow_run(session, target,info_string):
     print('%s: %s across %d steps, %.3f +/- %.3f sec / batch' %
           (datetime.now(), info_string, num_batchs, mn, sd))
 
+
 def run_benchmark():
     with tf.Graph().as_default():
         image_size = 299
         batch_size = 32
         images = tf.Variable(tf.random_normal([batch_size,
                                                image_size,
-                                               image_size,3],
+                                               image_size, 3],
                                               dtype=tf.float32,
                                               stddev=0.1))
         with slim.arg_scope(inception_v3_arg_scope()):
-            logits,end_points = inception_v3(images,is_training=False)
-
+            logits, end_points = inception_v3(images, is_training=False)
 
         init = tf.global_variables_initializer()
         sess = tf.Session()
         sess.run(init)
 
-        time_tensorflow_run(sess,logits,'Forward')
+        time_tensorflow_run(sess, logits, 'Forward')
+
 
 run_benchmark()

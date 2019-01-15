c59968ab414ba4835a12ed18b53567062a8195d2
ycccccccccc <1078988764@qq.com>
2019-01-15 13:19:01 +0800

    1.15

--- tensorflow/test.py
+++ tensorflow/test.py
@@ -1,15 +1,30 @@
-import collections
 import numpy as np
-import random
 import matplotlib.pyplot as plt
+from math import *
 
-plt.figure(figsize=(8, 8))
-x, y = 5,5
-plt.scatter(x, y)
-plt.annotate('qqq',
-             xy=(x, y),
-             xytext=(0, 0),
-             textcoords='offset points',
-             ha='center',
-             va='center')
+num = 100000
+x_range = 250
+
+def norm(m,n):
+    dot = np.random.normal(m,n,num)
+    xx = [int(x) for x in dot]
+    return xx
+a = np.zeros([11,100000])
+for i in range(10):
+    a[i] = norm(10+i*2,1+i/5.0)
+
+for i in range(num):
+    a[10,i] = sum(a[0:10,i])
+
+count = np.zeros([11,x_range])
+for con in range(11):
+    for i in a[con]:
+        count[con,int(i+20)] += 1
+
+x = range(x_range)
+x = [i - 20 for i in x]
+
+for i in range(11):
+    y = [y/num for y in count[i]]
+    plt.plot(x,y)
 plt.show()
--- useful/keras_augu_data.py
+++ useful/keras_augu_data.py
@@ -1,31 +1,50 @@
-from keras.preprocessing.image import ImageDataGenerator
-from keras.datasets import mnist
-from PIL import Image
-import matplotlib as pyplot
-import numpy as np
-import scipy
+# from keras.preprocessing.image import ImageDataGenerator
+# from keras.datasets import mnist
+# from PIL import Image
+# import matplotlib as pyplot
+# import numpy as np
+# import scipy
 import os
-import cv2 as cv
+import shutil
+# import cv2 as cv
 from keras.preprocessing import image
 
-# RANGE_DIR = [0,1,2,3,4,5,6,7,8,9,"A","C","D","E","F","K","L","M","R","S","T","U","V","X","Y"]
+ddir = './classed_114/'
+ssdir = './tmp/'
+
 RANGE_DIR = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'S', 'E', 'P', 'H', 'M', 'B', 'D', 'J', 'I', 'X', 'Z', 'T', 'A', 'K', 'V',
-             'U', 'Y', 'C', 'L','F', 'R']
+             'U', 'Y', 'C', 'L', 'F', 'R']
+
+
 shift = 0.2
-# width_shift_range=shift,height_shift_range=shift
-# rotation_range = 20
-# channel_shift_range = 100
-# zoom_range = 0.5
-# shear_range = 0.5
-datagen = image.ImageDataGenerator(shear_range = 0.5)
-for i in RANGE_DIR:
-    dir = "./1.3_pre_aug/%s/" % i
-    sdir = "./tmp/%s/" % i
-    len1 = len(os.listdir(dir+str(i)))
-    print(len1)
-    if not os.path.exists(sdir):
-        os.makedirs(sdir)
-    gen_data = datagen.flow_from_directory(dir, batch_size=1, shuffle=False, save_to_dir=sdir,
-                                           save_prefix='shear', target_size=(32, 32))
-    for j in range(len1):
-        gen_data.next()
+arg = {"shift": {"width_shift_range": shift, "height_shift_range": shift},
+       "rotate": {"rotation_range": 20},
+       "channel": {"channel_shift_range": 100},
+       "zoom": {"zoom_range": 0.5},
+       "shear": {"shear_range": 0.5}}
+
+def mkd():
+    for i in os.listdir(ddir):
+        path = os.path.join(ddir,i)
+        os.mkdir(os.path.join(path,i))
+        for j in os.listdir(path):
+            file = os.path.join(path,j)
+            if os.path.isfile(file):
+                shutil.move(file,os.path.join(path,i,j))
+
+def aug(method):
+    datagen = image.ImageDataGenerator(**arg[method])
+    for i in os.listdir(ddir):
+        dir = os.path.join(ddir,i)
+        sdir = os.path.join(ssdir,i)
+        len1 = len(os.listdir(os.path.join(dir,i)))
+        print(len1)
+        if not os.path.exists(sdir):
+            os.makedirs(sdir)
+        gen_data = datagen.flow_from_directory(dir, batch_size=1, shuffle=False, save_to_dir=sdir,
+                                               save_prefix=method, target_size=(32, 32))
+        for j in range(len1):
+            gen_data.next()
+
+for i in arg:
+    aug(i)
